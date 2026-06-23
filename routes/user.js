8dd7e88dfeec1df9b904e4ac1821d8a9328757f8
pranav <pranavtambe9404@gmai.com>
2026-06-23 23:45:04 +0530

    Initial backend workshop project

---
+++ routes/user.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router();
+
+const userController = require("../controllers/user");
+
+const { createUserValidator } = require("../validators/user");
+const validateInput = require("../validators/validateInput");
+const verifyToken = require("../middlewares/auth");
+
+
+router.post(
+    "/create",
+    createUserValidator,
+    validateInput,
+    userController.createUser
+);
+
+
+router.post(
+    "/login",
+    userController.login
+);
+
+
+router.get(
+    "/all",
+    verifyToken,
+    userController.getAllUsers
+);
+
+
+router.get(
+    "/:id",
+    userController.getUserById
+);
+
+
+router.put(
+    "/update/:id",
+    verifyToken,
+    userController.updateUser
+);
+
+
+router.delete(
+    "/delete/:id",
+    userController.deleteUser
+);
+
+module.exports = router;
